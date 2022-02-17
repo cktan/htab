c125bb5a3f081a8353739678ba8ca52ea58073f2
Akon <amwork@zoho.com>
2022-02-17 16:53:25 +0300

    Removed some dead code; make things more clear; erase() added

--- StringHashTable/main.cpp
+++ StringHashTable/main.cpp
@@ -8,26 +8,113 @@
 
 int main(int /*argc*/, char */*argv*/[]) {
   try {
-    static const size_t min_bucket_count = 20;
-    StringHashTable sht(min_bucket_count);
-    std::cout << "sht.size() = " << sht.size() << std::endl;
+    static const size_t min_bucket_count = 4;
+    string_hash_table_t sht(min_bucket_count);
+    std::cout << "size = " << sht.size() << std::endl;
 
-    const char str0[] = "str0";
-    StringRef sr0(str0, sizeof(str0));
+    const char s0[] = "";
+    const char s1[] = "01234567";
+    const char s2[] = "0123456701234567";
+    const char s3[] = "012345670123456701234567";
+    const char s4[] = "01234567012345670123456701234567";
+    StringRef sr0(s0, sizeof(s0));
+    StringRef sr1(s1, sizeof(s1));
+    StringRef sr2(s2, sizeof(s2));
+    StringRef sr3(s3, sizeof(s3));
+    StringRef sr4(s4, sizeof(s4));
 
-    const char str1[] = "str1";
-    StringRef sr1(str1, sizeof(str1));
+    std::pair<string_hash_table_t::value_holder, bool> insertion;
 
-    {
-      auto [where, inserted] = sht.emplace(sr0);  // or emplace -> insert
-      std::cout << std::boolalpha << "sr0 inserted = " << inserted << std::endl;
-      std::cout << "sht.size() = " << sht.size() << std::endl;
-    }
-    {
-      auto [where, inserted] = sht.insert(sr0);
-      std::cout << std::boolalpha << "sr0 inserted = " << inserted << std::endl;
-      std::cout << "sht.size() = " << sht.size() << std::endl;
-    }
+    // Insert values
+    std::cout << "Insert values:\n";
+
+    insertion = sht.insert(sr0);
+    std::cout << std::boolalpha << "sr0 inserted = " << insertion.second << std::endl;
+
+    insertion = sht.insert(sr1);
+    std::cout << std::boolalpha << "sr1 inserted = " << insertion.second << std::endl;
+
+    insertion = sht.insert(sr2);
+    std::cout << std::boolalpha << "sr2 inserted = " << insertion.second << std::endl;
+
+    insertion = sht.insert(sr3);
+    std::cout << std::boolalpha << "sr3 inserted = " << insertion.second << std::endl;
+
+    insertion = sht.insert(sr4);
+    std::cout << std::boolalpha << "sr4 inserted = " << insertion.second << std::endl;
+
+    std::cout << "size = " << sht.size() << std::endl;
+
+    // Insert again
+    std::cout << "Insert values again:\n";
+
+    insertion = sht.insert(sr0);
+    std::cout << std::boolalpha << "sr0 inserted = " << insertion.second << std::endl;
+
+    insertion = sht.insert(sr1);
+    std::cout << std::boolalpha << "sr1 inserted = " << insertion.second << std::endl;
+
+    insertion = sht.insert(sr2);
+    std::cout << std::boolalpha << "sr2 inserted = " << insertion.second << std::endl;
+
+    insertion = sht.insert(sr3);
+    std::cout << std::boolalpha << "sr3 inserted = " << insertion.second << std::endl;
+
+    insertion = sht.insert(sr4);
+    std::cout << std::boolalpha << "sr4 inserted = " << insertion.second << std::endl;
+
+    // Find values
+
+    std::cout << "Find values:\n";
+    string_hash_table_t::value_holder where;
+
+    where = sht.find(sr0);
+    std::cout << std::boolalpha << "sr0 contained = " << (sht.end() != where) << std::endl;
+
+    where = sht.find(sr1);
+    std::cout << std::boolalpha << "sr1 contained = " << (sht.end() != where) << std::endl;
+
+    where = sht.find(sr2);
+    std::cout << std::boolalpha << "sr2 contained = " << (sht.end() != where) << std::endl;
+
+    where = sht.find(sr3);
+    std::cout << std::boolalpha << "sr3 contained = " << (sht.end() != where) << std::endl;
+
+    where = sht.find(sr4);
+    std::cout << std::boolalpha << "sr4 contained = " << (sht.end() != where) << std::endl;
+
+    // Erase values
+
+    std::cout << "Erase values:\n";
+    sht.erase(sr0);
+    sht.erase(sr1);
+    sht.erase(sr2);
+    sht.erase(sr3);
+    sht.erase(sr4);
+    std::cout << "size = " << sht.size() << std::endl;
+
+    string_hash_table_t sht1;
+    string_hash_table_t sht2;
+
+    sht1 = std::move(sht1);
+    string_hash_table_t sht3(std::move(sht1));
+
+    return 0;
+  }
+  catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
+  catch (...) {
+    std::cerr << "Unknown application error" << std::endl;
+  }
+
+  return -1;
+}
+
+
+/* ==TRASH==
+
+
     {
       StringHashTable::iterator where = sht.find(sr0);
       bool contained = sht.end() != where;
@@ -52,20 +139,6 @@ int main(int /*argc*/, char */*argv*/[]) {
       std::cout << "sht.size() = " << sht.size() << std::endl;
     }
 
-    return 0;
-  }
-  catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
-  }
-  catch (...) {
-    std::cerr << "Unknown application error" << std::endl;
-  }
-
-  return -1;
-}
-
-
-/* ==TRASH==
 //#include <Common/HashTable/StringHashTable.h>
     std::cout << "sizeof(std::string) = " << sizeof(std::string) << std::endl;
     std::cout << "sizeof(std::string_view) = " << sizeof(std::string_view) << std::endl;
--- StringHashTable/string_hash_table.hpp
+++ StringHashTable/string_hash_table.hpp
@@ -8,6 +8,7 @@
 #include <unordered_set>
 #include <smmintrin.h>
 
+//TODO: remove ALWAYS_INLINE
 #define ALWAYS_INLINE __attribute__((__always_inline__))
 
 #define CASE_1_8 \
@@ -87,19 +88,19 @@ struct StringKey24
 
 inline StringRef ALWAYS_INLINE toStringRef(const StringKey0 &) { return {}; }
 
-inline StringRef ALWAYS_INLINE toStringRef(const StringKey8 & n) {
+inline StringRef ALWAYS_INLINE toStringRef(const StringKey8 &n) {
   return {reinterpret_cast<const char *>(&n), 8ul - (__builtin_clzll(n) >> 3)};
 }
 
-inline StringRef ALWAYS_INLINE toStringRef(const StringKey16 & n) {
+inline StringRef ALWAYS_INLINE toStringRef(const StringKey16 &n) {
   return {reinterpret_cast<const char *>(&n), 16ul - (__builtin_clzll(n.b) >> 3)};
 }
 
-inline StringRef ALWAYS_INLINE toStringRef(const StringKey24 & n) {
+inline StringRef ALWAYS_INLINE toStringRef(const StringKey24 &n) {
   return {reinterpret_cast<const char *>(&n), 24ul - (__builtin_clzll(n.c) >> 3)};
 }
 
-inline const StringRef & ALWAYS_INLINE toStringRef(const StringRef & s) {
+inline const StringRef & ALWAYS_INLINE toStringRef(const StringRef &s) {
   return s;
 }
 
@@ -131,8 +132,8 @@ struct StringHashTableHash
   size_t ALWAYS_INLINE operator()(StringRef key) const {
     size_t res = size_t(-1ULL);
     size_t sz = key.size;
-    const char * p = key.data;
-    const char * lp = p + sz - 8; // starting pointer of the last 8 bytes segment
+    const char *p = key.data;
+    const char *lp = p + sz - 8; // starting pointer of the last 8 bytes segment
     char s = (-sz & 7) * 8; // pending bits that needs to be shifted out
     UInt64 n[3]; // StringRef in SSO map will have length > 24
     memcpy(&n, p, 24);
@@ -140,8 +141,7 @@ struct StringHashTableHash
     res = _mm_crc32_u64(res, n[1]);
     res = _mm_crc32_u64(res, n[2]);
     p += 24;
-    while (p + 8 < lp)
-    {
+    while (p + 8 < lp) {
       memcpy(&n[0], p, 8);
       res = _mm_crc32_u64(res, n[0]);
       p += 8;
@@ -162,6 +162,8 @@ struct StringHashTableEmpty
   bool is_empty{true};
 
   StringHashTableEmpty() { memset(reinterpret_cast<char *>(&value), 0, sizeof(value)); }
+
+  using key_type = Cell;
 
   template <bool is_const>
   struct iterator_base
@@ -219,6 +221,12 @@ struct StringHashTableEmpty
     return {begin(), inserted};
   }
 
+  size_t ALWAYS_INLINE erase(const Cell &) {
+    if (is_empty) return 0;
+    is_empty = false;
+    return 1;
+  }
+
   template <typename Key>
   iterator ALWAYS_INLINE find(Key) { return begin(); }
 
@@ -241,177 +249,174 @@ struct StringHashTableEmpty
   size_t getCollisions() const { return 0; }
 };
 
-class StringHashTable
-{
-private:
-  typedef StringHashTableEmpty<StringKey0> T0;
-  typedef std::unordered_set<StringKey8,  StringHashTableHash> T1;
-  typedef std::unordered_set<StringKey16, StringHashTableHash> T2;
-  typedef std::unordered_set<StringKey24, StringHashTableHash> T3;
-  typedef std::unordered_set<StringRef,   StringHashTableHash> Ts;
-
-  T0 m0;
-  T1 m1;
-  T2 m2;
-  T3 m3;
-  Ts ms;
-
+class string_hash_table_t {
 public:
-  using Key = StringRef;
-  using key_type = Key;
-  using value_type = typename Ts::value_type;
-
-  StringHashTable(const StringHashTable&) = delete;
-  StringHashTable& operator=(const StringHashTable&) = delete;
-
-  StringHashTable() {}
-  StringHashTable(size_t reserve_for_num_elements)
+  string_hash_table_t() {}
+  string_hash_table_t(size_t min_bucket_count)
     : m0()
-    , m1(reserve_for_num_elements / 4)
-    , m2(reserve_for_num_elements / 4)
-    , m3(reserve_for_num_elements / 4)
-    , ms(reserve_for_num_elements / 4)
+    , m1(min_bucket_count / 4)
+    , m2(min_bucket_count / 4)
+    , m3(min_bucket_count / 4)
+    , ms(min_bucket_count / 4)
   {
   }
 
-  //TODO:disabled
-//  StringHashTable(StringHashTable &&rhs) { *this = std::move(rhs); }
-//  ~StringHashTable() {}
+  using value_type = StringRef;
 
-  //TODO: remove ValueHolder: use StringRef directly
-  struct ValueHolder
-  {
-    StringRef value;
-    auto *operator-> () { return this; }
-    value_type getValue() const { return value; }
-    ValueHolder() : value{} {}
+  // Public interface cannot return iterator like STL containers (e.g.
+  // iterator unordered_set::find(const key_type &k);), due to implementation
+  // details - there are several internal containers of different value/key
+  // types. Following STL style we return this wrapper.
+  class value_holder {
+  public:
+    value_holder() : value() {}
     template <typename Iterator>
-    //src:ValueHolder(const Iterator & iter) : value(toStringRef(iter->getValue())) {}
-    ValueHolder(const Iterator &iter) : value(toStringRef(*iter)) {}
+    value_holder(const Iterator &it) : value(toStringRef(*it)) {}
+    auto *operator->() { return this; }
     template <typename Iterator>
-    void operator=(const Iterator &iter) {
-      //src:value = toStringRef(iter->getValue());
-      value = toStringRef(*iter);
-    }
+    void operator=(const Iterator &it) { value = toStringRef(*it); }
     // Only used to check if it's end() in find
-    bool operator==(const ValueHolder &that) const { return value.size == 0 && that.value.size == 0; }
-    bool operator!=(const ValueHolder &that) const { return !(*this == that); }
+    bool operator==(const value_holder &that) const {
+      return value.size == 0 && that.value.size == 0;
+    }
+    bool operator!=(const value_holder &that) const { return !(*this == that); }
+  private:
+    StringRef value;
   };
-  using iterator = ValueHolder;
 
-  ValueHolder ALWAYS_INLINE end() { return ValueHolder{}; }
+  value_holder end() { return value_holder(); }
   size_t size() const { return m0.size() + m1.size() + m2.size() + m3.size() + ms.size(); }
   bool empty() const { return m0.empty() && m1.empty() && m2.empty() && m3.empty() && ms.empty(); }
 
+  inline value_holder ALWAYS_INLINE find(value_type v);
+  inline std::pair<value_holder, bool> ALWAYS_INLINE insert(value_type v);
+  inline size_t ALWAYS_INLINE erase(value_type v);
+
+private:
+  StringHashTableEmpty<StringKey0> m0;
+  std::unordered_set<StringKey8,  StringHashTableHash> m1;
+  std::unordered_set<StringKey16, StringHashTableHash> m2;
+  std::unordered_set<StringKey24, StringHashTableHash> m3;
+  std::unordered_set<StringRef,   StringHashTableHash> ms;
+
+  template <typename Func>
+  inline decltype(auto) ALWAYS_INLINE dispatch(value_type x, Func func);
+
+  struct find_callable {
+    template <typename Map>
+    value_holder ALWAYS_INLINE operator()(Map &map, const typename Map::key_type &key) {
+      typename Map::iterator it = map.find(key);
+      return it != map.end() ? value_holder(it) : value_holder();
+    }
+  };
+
+  struct insert_callable {
+    template <typename Map>
+    std::pair<value_holder, bool> ALWAYS_INLINE operator()(Map &map,
+                                                          const typename Map::key_type &key) {
+      auto [it, inserted] = map.insert(key);
+      return {it != map.end() ? value_holder(it) : value_holder(), inserted};
+    }
+  };
+
+  struct erase_callable {
+    template <typename Map>
+    size_t ALWAYS_INLINE operator()(Map &map, const typename Map::key_type &key) {
+      return map.erase(key);
+    }
+  };
+};
+
+template <typename Func>
+decltype(auto) string_hash_table_t::dispatch(value_type x, Func func) {
   // Dispatch is written in a way that maximizes the performance:
   // 1. Always memcpy 8 times bytes
   // 2. Use switch case extension to generate fast dispatching table
   // 3. [DELETED] Combine hash computation along with key loading
   // 4. Funcs are named callables that can be force_inlined
   // NOTE: It relies on Little Endianness and SSE4.2
-  template <typename Func>
-  decltype(auto) ALWAYS_INLINE dispatch(Key x, Func func)
-  {
-    static constexpr StringKey0 key0{};
-    size_t sz = x.size;
-    const char * p = x.data;
-    // pending bits that needs to be shifted out
-    char s = (-sz & 7) * 8;
-    union
-    {
-      StringKey8 k8;
-      StringKey16 k16;
-      StringKey24 k24;
-      UInt64 n[3];
-    };
-    switch (sz)
-    {
-    case 0:
-        return func(m0, key0);
+  static constexpr StringKey0 key0;
+  size_t sz = x.size;
+  const char *p = x.data;
+  char s = (-sz & 7) * 8; // pending bits that needs to be shifted out
+  union {
+    StringKey8 k8;
+    StringKey16 k16;
+    StringKey24 k24;
+    UInt64 n[3];
+  };
+  switch (sz) {
+    case 0: {
+      return func(m0, key0);
+    }
     CASE_1_8 : {
-        // first half page
-        if ((reinterpret_cast<uintptr_t>(p) & 2048) == 0)
-        {
-          memcpy(&n[0], p, 8);
-          n[0] &= -1ul >> s;
-        }
-        else
-        {
-          const char * lp = x.data + x.size - 8;
-          memcpy(&n[0], lp, 8);
-          n[0] >>= s;
-        }
-        return func(m1, k8);
-      }
-    CASE_9_16 : {
+      if ((reinterpret_cast<uintptr_t>(p) & 2048) == 0) { // first half page
         memcpy(&n[0], p, 8);
-        const char * lp = x.data + x.size - 8;
-        memcpy(&n[1], lp, 8);
-        n[1] >>= s;
-        return func(m2, k16);
+        n[0] &= -1ul >> s;
       }
-    CASE_17_24 : {
-        memcpy(&n[0], p, 16);
-        const char * lp = x.data + x.size - 8;
-        memcpy(&n[2], lp, 8);
-        n[2] >>= s;
-        return func(m3, k24);
-      }
-      default: {
-        memcpy(&n, x.data, 24);
-        p += 24;
-        const char * lp = x.data + x.size - 8;
-        while (p + 8 < lp)
-        {
-          memcpy(&n[0], p, 8);
-          p += 8;
-        }
+      else {
+        const char *lp = x.data + x.size - 8;
         memcpy(&n[0], lp, 8);
         n[0] >>= s;
-        return func(ms, x);
       }
+      return func(m1, k8);
+    }
+    CASE_9_16 : {
+      memcpy(&n[0], p, 8);
+      const char *lp = x.data + x.size - 8;
+      memcpy(&n[1], lp, 8);
+      n[1] >>= s;
+      return func(m2, k16);
+    }
+    CASE_17_24 : {
+      memcpy(&n[0], p, 16);
+      const char *lp = x.data + x.size - 8;
+      memcpy(&n[2], lp, 8);
+      n[2] >>= s;
+      return func(m3, k24);
+    }
+    default: {
+      return func(ms, x);
     }
   }
+}
 
-  //std: iterator find ( const key_type& k );
-  struct FindCallable {
-    template <typename Map, typename Key>
-    ValueHolder ALWAYS_INLINE operator()(Map &map, const Key &x) {
-      typename Map::iterator it = map.find(x);
-      return it != map.end() ? ValueHolder(it) : ValueHolder();
-    }
-  };
-  ValueHolder ALWAYS_INLINE find(Key x) {
-    return dispatch(x, FindCallable{});
-  }
+// std: iterator find(const key_type &k);
+string_hash_table_t::value_holder string_hash_table_t::find(value_type v) {
+  return dispatch(v, find_callable());
+}
 
-  //std: pair<iterator,bool> insert ( const value_type& val );
-  struct InsertCallable {
-    template <typename Map, typename Key>
-    std::pair<ValueHolder, bool> ALWAYS_INLINE operator()(Map &map, const Key &x) {
-      auto [it, inserted] = map.insert(x);
-      return {it != map.end() ? ValueHolder(it) : ValueHolder(), inserted};
-    }
-  };
-  std::pair<ValueHolder, bool> ALWAYS_INLINE insert(Key x) {
-    return dispatch(x, InsertCallable{});
-  }
+// std: pair<iterator, bool> insert(const value_type &val);
+std::pair<string_hash_table_t::value_holder, bool> string_hash_table_t::insert(value_type v) {
+  return dispatch(v, insert_callable());
+}
+
+// std: size_type erase(const key_type &k);
+inline size_t ALWAYS_INLINE string_hash_table_t::erase(value_type v) {
+  return dispatch(v, erase_callable());
+}
+
+
+/* ==TRASH==
+//  string_hash_table_t(const string_hash_table_t &) = delete;
+//  string_hash_table_t &operator=(const string_hash_table_t &) = delete;
+
+//  string_hash_table_t(string_hash_table_t &&) = default;
+//  string_hash_table_t &operator=(string_hash_table_t &&) = default;
 
   //std: template <class... Args> pair <iterator,bool> emplace ( Args&&... args );
   struct EmplaceCallable {
     template <typename Map, typename Key>
-    std::pair<ValueHolder, bool> ALWAYS_INLINE operator()(Map &map, const Key &x) {
-      auto [it, inserted] = map.emplace(x);
-      return {it != map.end() ? ValueHolder(it) : ValueHolder(), inserted};
+    std::pair<value_holder, bool> ALWAYS_INLINE operator()(Map &map,
+                                                          const typename Map::key_type &key) {
+      auto [it, inserted] = map.emplace(key);
+      return {it != map.end() ? value_holder(it) : value_holder(), inserted};
     }
   };
-  std::pair<ValueHolder, bool> ALWAYS_INLINE emplace(Key x) {
-    return dispatch(x, EmplaceCallable{});
+  std::pair<value_holder, bool> ALWAYS_INLINE emplace(value_type v) {
+    return dispatch(v, EmplaceCallable());
   }
-};
 
-
-/* ==TRASH==
 //template<>
 //struct std::hash<StringKey0> {
 //  size_t ALWAYS_INLINE operator()(StringKey0) const noexcept { return 0; }
@@ -420,9 +425,9 @@ public:
   //src:
 //  struct EmplaceCallable
 //  {
-//    ValueHolder & it;
+//    value_holder & it;
 //    bool & inserted;
-//    EmplaceCallable(ValueHolder & it_, bool & inserted_) : it(it_), inserted(inserted_) {}
+//    EmplaceCallable(value_holder & it_, bool & inserted_) : it(it_), inserted(inserted_) {}
 //    template <typename Map, typename Key>
 //    void ALWAYS_INLINE operator()(Map & map, const Key & x, size_t hash)
 //    {
@@ -431,5 +436,5 @@ public:
 //      it = impl_it;
 //    }
 //  };
-//  void ALWAYS_INLINE emplace(Key x, ValueHolder & it, bool & inserted) { dispatch(x, EmplaceCallable{it, inserted}); }
+//  void ALWAYS_INLINE emplace(Key x, value_holder & it, bool & inserted) { dispatch(x, EmplaceCallable{it, inserted}); }
 */
